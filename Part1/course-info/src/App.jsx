1f4c10327ed626587cc0548aeb4170dabaa0d965
ronikauppinen <80982932+ronikauppinen@users.noreply.github.com>
2024-01-25 09:43:36 +0200

    Working on part1

---
+++ Part1/course-info/src/App.jsx
@@ -0,0 +1,52 @@
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+      <div>
+      <h1>{course.name}</h1>
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
--- Part1/part1/src/App.jsx
+++ Part1/part1/src/App.jsx
@@ -4,36 +4,51 @@ import './App.css';
 const Footer = () => {
   return (
     <div>
-      greeting app created by <a href='https://github.com/ronikauppinen'>Roni</a>
+      greeting app created by{' '}
+      <a href="https://github.com/ronikauppinen">Roni</a>
     </div>
-  )
-}
+  );
+};
 
-  const Hello = (props) => {
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
 
-    console.log(props)
-    return (
+const App = () => {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ];
+
+  const name = 'Peter';
+  const age = 10;
+
+  return (
+    <>
       <div>
-        <p>
-  
-          Hello {props.name}, you are {props.age} years old
-        </p>
-      </div>
-    )
-  }
-  
-  const App = () => {
-  
-    const name = 'Peter'
-    const age = 10
-  
-    return (
-      <><div>
         <h1>Greetings</h1>
 
-        <Hello name='Maya' age={26 + 10} />
+        <div>
+          <p>
+            {friends[0].name} {friends[0].age}
+          </p>
+          <p>
+            {friends[1].name} {friends[1].age}
+          </p>
+        </div>
+
+        <Hello name="Maya" age={26 + 10} />
         <Hello name={name} age={age} />
-      </div><Footer /></>
-    )
-  }
+      </div>
+      <Footer />
+    </>
+  );
+};
 export default App;
