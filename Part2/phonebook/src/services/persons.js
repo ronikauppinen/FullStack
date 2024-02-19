fc926736415f59e1aab3235f7fa8c644e1d9a7d3
ronikauppinen <80982932+ronikauppinen@users.noreply.github.com>
2024-02-19 13:39:46 +0200

    Till 3.9

--- Part2/phonebook/src/services/persons.js
+++ Part2/phonebook/src/services/persons.js
@@ -1,23 +1,28 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = 'http://localhost:3001/api/persons';
 
 const getAllPersons = () => {
   return axios.get(baseUrl).then(response => response.data);
 };
+
 const addPerson = (newPerson) => {
   return axios.post(baseUrl, newPerson).then(response => response.data);
 };
+
 const deletePerson = (id) => {
-  return axios.delete(`${baseUrl}/${id}`);
+  return axios.delete(`${baseUrl}/${id}`).then(response => response.data);
 };
+
 const updatePerson = (id, updatedPerson) => {
   return axios.put(`${baseUrl}/${id}`, updatedPerson)
     .then(response => response.data);
 };
+
 const services = {
   getAllPersons,
   addPerson,
   deletePerson,
   updatePerson,
 };
+
 export default services;
--- Part3/index.js
+++ Part3/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
 
+app.use(cors());
 
 //Custom token
 morgan.token('postData', (req, res) => {
